732c2093786b2151aed12c411345002819ffc6f4
Facundo Olmedo <olmedofacundogabriel@gmail.com>
2021-11-18 00:03:57 -0300

    editando App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Probando React
+          Probando React con los cambios de git
         </p>
         <a
           className="App-link"
