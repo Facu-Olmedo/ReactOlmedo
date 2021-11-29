a8d11fa9ca7bf774403e02a2d8b2cd38f66be057
Facundo Olmedo <olmedofacundogabriel@gmail.com>
2021-11-29 08:07:14 -0300

    practica 5

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import BarraNav from './componentes/NavBar/NavBar.js'
 import ItemComponentContainer from './componentes/ItemComponentContainer/ItemComponentContainer.js';
-import ItemCount from './componentes/ItemCount/ItemCount';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <header className="App-header">
         <ItemComponentContainer greeting="¡Bienvenido a la Tienda Virtual!"/>
         <img src={logo} className="App-logo" alt="logo" />
-        <ItemCount stock={20} inicial={1} />
       </header>
     </div>
   );
--- src/componentes/ItemComponentContainer/ItemComponentContainer.js
+++ src/componentes/ItemComponentContainer/ItemComponentContainer.js
@@ -1,7 +1,10 @@
+import ItemCount from "../ItemCount/ItemCount.js"
+
 function ItemComponentContainer({greeting}){
     return(
         <div>
             <h1>{greeting}</h1>
+            <ItemCount stock={20} inicial={1} />
         </div>
     )
 }
